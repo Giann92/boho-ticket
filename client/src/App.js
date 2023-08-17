3653c4de34e5d5fa93bb1919bf9616c25e8637fd
Giann92 <56614039+Giann92@users.noreply.github.com>
2023-08-17 03:25:52 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ client/src/App.js
@@ -0,0 +1,37 @@
+import './App.css';
+//DEPENDENCIES
+import axios from 'axios';
+import { Route, Routes } from 'react-router-dom';
+
+//Components
+import Home from './Components/Home/Home';
+import Register from './Components/register/register';
+import Login from './Components/login/login';
+import FormEvent from './Components/FormEvent/FormEvent';
+import Detail from './Components/Detail/Detail';
+import Nav from './Components/Nav/Nav';
+import Footer from './Components/Footer/Footer';
+import TermsAndConditions from './Components/TermsAndConditions/TermsAndConditions';
+import FAQ from './Components/FAQs/FAQs';
+axios.defaults.baseURL = 'https://pf-grupo06-back.onrender.com';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/login' element={<Login />}/> 
+          <Route path='/register' element={<Register />} />
+          <Route path='/event/:id' element={<Detail/>}/>
+          <Route path='/createEvent' element={<FormEvent/>}/>
+          <Route path='/TaC' element={<TermsAndConditions/>}/>
+          <Route path='/FAQ' element={<FAQ/>}/>
+        </Routes>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
